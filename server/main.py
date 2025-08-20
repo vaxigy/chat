a306bff2240bae9d16959302750ccba8e46f433d
vaxigy <vip.vaxyly@gmail.com>
2025-08-20 16:17:15 +0300

    refactor: extract server startup into main() function

--- server/main.py
+++ server/main.py
@@ -1,6 +1,11 @@
 from core.runner import ChatRunner
 from core.config import SERVER_HOST, SERVER_PORT
 
-if __name__ == '__main__':
+
+def main():
     server = ChatRunner(SERVER_HOST, SERVER_PORT)
     server.run()
+
+
+if __name__ == '__main__':
+    main()
